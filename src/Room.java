72fcad41f50a0762d90199d8a507f94d8b09769e
Fahim Mohammed <mohammedf2606@gmail.com>
2018-11-28 19:18:06 +0000

    Format

--- src/Room.java
+++ src/Room.java
@@ -3,16 +3,13 @@ import java.util.Set;
 
 /**
  * Class Room - a room in an adventure game.
- * <p>
- * This class is part of the "World of Zuul" application.
- * "World of Zuul" is a very simple, text based adventure game.
- * <p>
+ *
  * A "Room" represents one location in the scenery of the game.  It is
  * connected to other rooms via exits.  For each existing exit, the room
  * stores a reference to the neighboring room.
  *
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author Michael Kölling, David J. Barnes and Fahim Mohammed
+ * @version 2018.11.28
  */
 
 public class Room {
@@ -74,102 +71,102 @@ public class Room {
     characters.remove(character.getName());
   }
 
-    /**
-     * @return The name of the room
-     * (the one that was defined in the constructor).
-     */
-    public String getName() {
-        return name;
-    }
+  /**
+   * @return The name of the room
+   * (the one that was defined in the constructor).
+   */
+  public String getName() {
+      return name;
+  }
 
-    /**
-     * @return The short description of the room
-     * (the one that was defined in the constructor).
-     */
-    String getShortDescription() {
-      return description;
-    }
+  /**
+   * @return The short description of the room
+   * (the one that was defined in the constructor).
+   */
+  String getShortDescription() {
+    return description;
+  }
 
-    /**
-     * Return a description of the room in the form:
-     * You are in the kitchen.
-     * Exits: north west
-     *
-     * @return A long description of this room
-     */
-    String getLongDescription() {
-        return "You are " + description + ".\n"
-        + getExitString() + getItemString() + getCharString();
-    }
+  /**
+   * Return a description of the room in the form:
+   * You are in the kitchen.
+   * Exits: north west
+   *
+   * @return A long description of this room
+   */
+  String getLongDescription() {
+      return "You are " + description + ".\n"
+      + getExitString() + getItemString() + getCharString();
+  }
 
-    /**
-     * Return a string describing the room's exits, for example
-     * "Exits: north west".
-     *
-     * @return Details of the room's exits.
-     */
-    private String getExitString() {
-        StringBuilder returnString = new StringBuilder("Exits:");
-        Set<String> keys = exits.keySet();
-        keys.forEach(e -> returnString.append(" ").append(e));
-        return returnString.toString();
-    }
+  /**
+   * Return a string describing the room's exits, for example
+   * "Exits: north west".
+   *
+   * @return Details of the room's exits.
+   */
+  private String getExitString() {
+      StringBuilder returnString = new StringBuilder("Exits:");
+      Set<String> keys = exits.keySet();
+      keys.forEach(e -> returnString.append(" ").append(e));
+      return returnString.toString();
+  }
 
-    /**
-     * Return the room that is reached if we go from this room in direction
-     * "direction". If there is no room in that direction, return null.
-     *
-     * @param direction The exit's direction.
-     * @return The room in the given direction.
-     */
-    Room getExit(String direction) {
-      return exits.get(direction);
-    }
+  /**
+   * Return the room that is reached if we go from this room in direction
+   * "direction". If there is no room in that direction, return null.
+   *
+   * @param direction The exit's direction.
+   * @return The room in the given direction.
+   */
+  Room getExit(String direction) {
+    return exits.get(direction);
+  }
 
-    /**
-     * Return a string describing the items in the room, for example
-     * "Items: beer".
-     *
-     * @return Details of the room's items.
-     */
-    private String getItemString() {
-        if (items.isEmpty()) {
-            return "";
-        }
-        StringBuilder returnString = new StringBuilder("\nItems:");
-        Set<String> keys = items.keySet();
-        keys.forEach(i -> returnString.append(" ").append(i));
-        return returnString.toString();
-    }
+  /**
+   * Return a string describing the items in the room, for example
+   * "Items: beer".
+   *
+   * @return Details of the room's items.
+   */
+  private String getItemString() {
+      if (items.isEmpty()) {
+          return "";
+      }
+      StringBuilder returnString = new StringBuilder("\nItems:");
+      Set<String> keys = items.keySet();
+      keys.forEach(i -> returnString.append(" ").append(i));
+      return returnString.toString();
+  }
 
-    /**
-     * Return a string describing the charcters in the room, for example
-     * "Character: Prof. Kölling".
-     *
-     * @return Details of the room's characters.
-     */
-    private String getCharString() {
-        if (characters.isEmpty()) {
-            return "";
-        }
-        StringBuilder returnString = new StringBuilder("\nCharacters:");
-        Set<String> keys = characters.keySet();
-        keys.forEach(c -> returnString.append(" ").append(c));
-        return returnString.toString();
-    }
+  /**
+   * Return a string describing the charcters in the room, for example
+   * "Character: Prof. Kölling".
+   *
+   * @return Details of the room's characters.
+   */
+  private String getCharString() {
+      if (characters.isEmpty()) {
+          return "";
+      }
+      StringBuilder returnString = new StringBuilder("\nCharacters:");
+      Set<String> keys = characters.keySet();
+      keys.forEach(c -> returnString.append(" ").append(c));
+      return returnString.toString();
+  }
 
-    /**
-     * Returns the characters in the current room
-     */
-    HashMap<String, Character> getCharacters()
-    {
-      return characters;
-    }
+  /**
+   * Returns the characters in the current room
+   */
+  HashMap<String, Character> getCharacters()
+  {
+    return characters;
+  }
 
-    /**
-     * Returns the items in the current room
-     */
-    HashMap<String, Item> getItems() {
-        return items;
-    }
+  /**
+   * Returns the items in the current room
+   */
+  HashMap<String, Item> getItems() {
+      return items;
+  }
 }
